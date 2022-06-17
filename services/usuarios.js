348224110ffc3261aa1ed4e6bd150dc35c4b6bc7
LanzerDev <78884322+LanzerDev@users.noreply.github.com>
2022-06-16 21:37:42 -0600

    sd

--- services/usuarios.js
+++ services/usuarios.js
@@ -46,8 +46,6 @@ class usuarios {
 
     createUser(data, callback) {
         const newUser = { ...data };
-        this.users.push(newUser)
-
         this.nombre = newUser.Nombre;
         this.apellido1 = newUser.Apellido_1;
         this.apellido2 = newUser.Apellido_2;
@@ -70,16 +68,7 @@ class usuarios {
         this.numero_personas_hogar = newUser.Personas_hogar;
         this.numero_personas_trabajaron = newUser.Personas_hogar_trabajaron;
 
-        const query = `INSERT INTO usuarios VALUES (NULL,'`+this.nombre+`','`+this.apellido1+`','`+this.apellido2+`','`+this.email+`','`+this.telefono+`','`+this.genero+`','`+this.fecha_nacimiento+`','`+this.edad+`','`+this.estado+`','`+this.municipio+`','`+this.nivel_estudios+`','`+this.carrera+`','`+this.ocupacion+`','`+this.nivel_ingresos+`','`+this.estado_civil+`','`+this.tiene_hijos+`','`+this.tiene_hijos_menores18+`','`+this.numero_automoviles+`','`+this.tiene_internet+`','`+this.numero_personas_hogar+`','`+this.numero_personas_trabajaron+`','...')` 
-
-  
-        // '${this.tiene_hijos}\r\n',
-        // '${this.tiene_hijos_menores18}\r\n',
-        // '${this.numero_automoviles}\r\n',
-        // '${this.tiene_internet}\r\n',
-        // '${this.numero_personas_hogar}\r\n',
-        // '${this.numero_personas_trabajaron}\r\n');`
-
+        const query = `INSERT INTO usuarios VALUES (NULL,'`+this.nombre+`','`+this.apellido1+`','`+this.apellido2+`','`+this.email+`','`+this.telefono+`','`+this.genero+`','`+this.fecha_nacimiento+`','`+this.edad+`','`+this.estado+`','`+this.municipio+`','`+this.nivel_estudios+`','`+this.carrera+`','`+this.ocupacion+`','`+this.nivel_ingresos+`','`+this.estado_civil+`','`+this.tiene_hijos+`','`+this.tiene_hijos_menores18+`','`+this.numero_automoviles+`','`+this.tiene_internet+`','`+this.numero_personas_hogar+`','`+this.numero_personas_trabajaron+`','...')`;
         const checkCorreo = `SELECT * FROM usuarios WHERE Correo LIKE "`+this.email+`%"`
         const checkNumero = `SELECT * FROM usuarios WHERE Telefono LIKE "`+this.telefono+`%"`
 
@@ -89,6 +78,7 @@ class usuarios {
             }
 
             if(res.length > 0){
+                console.log('correo ya en uso')
                 callback('Este correo ya a sido registrado', "0")
             } else {
                 conexion.query(checkNumero, (error, res, fields)=>{
@@ -96,9 +86,11 @@ class usuarios {
                         throw error
                     }
                     if(res.length > 0){
+                        console.log('telefono ya en uso')
                         callback('Este numero ya sido registrado', "1")
                     } else {
                         conexion.query(query) 
+                        console.log("usuario registrado")
                         callback('Usuario registrado correctamente', "2")
                     }
                 })
