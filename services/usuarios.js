e804d5254da47ce3c5e823aeb567d0436d001333
LanzerDev <hdsnpej@gmail.com>
2022-09-10 00:19:52 -0600

    laptop new ingreso

--- services/usuarios.js
+++ services/usuarios.js
@@ -34,7 +34,7 @@ class usuarios {
         this.numero_personas_trabajaron;
         this.comentarios;
     }
-//laptop update
+    //laptop update
     getUsuarios(call) {
         const get_usuarios = 'SELECT * FROM usuarios';
         conexion.query(get_usuarios, (err, res, fields) => {
@@ -74,18 +74,20 @@ class usuarios {
         this.numero_personas_hogar = newUser.Personas_hogar;
         this.numero_personas_trabajaron = newUser.Personas_hogar_trabajaron;
 
-        const query = `INSERT INTO usuarios VALUES (NULL,'`+this.nombre+`','`+this.apellido1+`','`+this.apellido2+`','`+this.email+`','`+this.telefono+`','`+this.genero+`','`+this.fecha_nacimiento+`','`+this.edad+`','`+this.estado+`','`+this.municipio+`','`+this.calle+`','`+this.numero+`','`+this.colonia+`','`+this.nivel_estudios+`','`+this.carrera+`','`+this.ocupacion+`','`+this.nivel_ingresos+`','`+this.estado_civil+`','`+this.tiene_hijos+`','`+this.tiene_hijos_menores18+`','`+this.numero_automoviles+`','`+this.tiene_internet+`','`+this.numero_personas_hogar+`','`+this.numero_personas_trabajaron+`','...')`;
-        const checkNumero = `SELECT * FROM usuarios WHERE Telefono LIKE "`+this.telefono+`%"`
+        const new_ingreso_mensual = newUser.Ingresos_mensual.replace(/[$MXNMILmil]/g, '') * 1000;
 
-        conexion.query(checkNumero, (error, res, fields)=>{
-            if(error){
+        const query = `INSERT INTO usuarios VALUES (NULL,'` + this.nombre + `','` + this.apellido1 + `','` + this.apellido2 + `','` + this.email + `','` + this.telefono + `','` + this.genero + `','` + this.fecha_nacimiento + `','` + this.edad + `','` + this.estado + `','` + this.municipio + `','` + this.calle + `','` + this.numero + `','` + this.colonia + `','` + this.nivel_estudios + `','` + this.carrera + `','` + this.ocupacion + `','` + new_ingreso_mensual + `','` + this.estado_civil + `','` + this.tiene_hijos + `','` + this.tiene_hijos_menores18 + `','` + this.numero_automoviles + `','` + this.tiene_internet + `','` + this.numero_personas_hogar + `','` + this.numero_personas_trabajaron + `','...')`;
+        const checkNumero = `SELECT * FROM usuarios WHERE Telefono LIKE "` + this.telefono + `%"`
+
+        conexion.query(checkNumero, (error, res, fields) => {
+            if (error) {
                 throw error
             }
-            if(res.length > 0){
+            if (res.length > 0) {
                 console.log('telefono ya en uso')
                 callback('Este numero ya sido registrado', "1")
             } else {
-                conexion.query(query) 
+                conexion.query(query)
                 console.log("usuario registrado")
                 callback('Usuario registrado correctamente', "2")
             }
@@ -94,25 +96,25 @@ class usuarios {
         return this.message_success;
     }
 
-    deleteUser(id, callback){
+    deleteUser(id, callback) {
         const delete_usuario = `DELETE FROM usuarios WHERE id_usuario = ${id}`;
-        conexion.query(delete_usuario, (err, res, field)=>{
-            if(err){
+        conexion.query(delete_usuario, (err, res, field) => {
+            if (err) {
                 throw err
             }
-            if(res.affectedRows > 0){
-                callback("usuario eliminado","0"); 
+            if (res.affectedRows > 0) {
+                callback("usuario eliminado", "0");
             } else {
                 callback("usuario no existe", "1")
             }
         })
     }
 
-    getUser(id, callback){
+    getUser(id, callback) {
         const get_usuario = `SELECT * FROM usuarios WHERE id_usuario = ${id}`;
 
-        conexion.query(get_usuario, (err, res, field)=>{
-            if(err){
+        conexion.query(get_usuario, (err, res, field) => {
+            if (err) {
                 throw err
             }
 
@@ -120,33 +122,33 @@ class usuarios {
         })
     }
 
-    saveComentarios(coment, id, callback){
+    saveComentarios(coment, id, callback) {
         const comentario = coment;
         const save_comentario = `UPDATE usuarios SET Comentarios = '${comentario}' WHERE id_usuario = '${id}'`
-        conexion.query(save_comentario, (err, res, field)=>{
-            if(err){
+        conexion.query(save_comentario, (err, res, field) => {
+            if (err) {
                 throw err
             }
-            if(res.changedRows > 0){
+            if (res.changedRows > 0) {
                 callback("Se actualizo el comentario c:")
             }
         })
     }
 
-    login(user, callback){
+    login(user, callback) {
         const usuario = user.usuario;
         const password = user.password;
         const checkUser = `SELECT * FROM administradores WHERE usuario LIKE '${usuario}'`
-        conexion.query(checkUser, (err, res, field)=>{
-            if(err){
+        conexion.query(checkUser, (err, res, field) => {
+            if (err) {
                 throw err
             }
-            if(res.length > 0){
-                if(password === res[0].password){
+            if (res.length > 0) {
+                if (password === res[0].password) {
                     callback("1", "login")
                 } else {
                     callback("0", "contraseña incorrecta")
-                } 
+                }
             } else {
                 callback("2", "Usuario no existe")
             }
